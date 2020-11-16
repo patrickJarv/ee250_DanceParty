69288b11fb5eaf095a5351fad2562433b49e8c5c
PatrickJarvis <pjarvis@usc.edu>
2020-11-15 21:28:23 -0800

    Update quit.py

--- modules/quit.py
+++ modules/quit.py
@@ -1,11 +1,9 @@
-#You can import any modules required here
 import os
-#This is name of the module - it can be anything you want
+
+#module that causes the program to quit
 moduleName = "quit"
 
-#These are the words you must say for this module to be executed
 commandWords = ["quit","control"]
 
-#This is the main function which will be execute when the above command words are said
 def execute(command):
     os._exit(0)
